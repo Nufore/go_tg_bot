e150f847a7dd7e37930fd8b2499a9805f49ae75b
Nufore <nufore@gmail.com>
2025-01-24 14:44:28 +0300

    implemented commands start help hello pickrandom

---
+++ events/telegram/messages.go
@@ -0,0 +1,12 @@
+package telegram
+
+const msgHelp = `Some help message to bot`
+
+const msgHello = "Hi there! \n\n" + msgHelp
+
+const (
+	msgUnknownCommand = "Unknown command"
+	msgNoSavedPages   = "You have no saved pages"
+	msgSaved          = "Saved!"
+	msgAlreadyExists  = "You already have this page in your list"
+)
